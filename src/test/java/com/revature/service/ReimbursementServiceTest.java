31db096d8d63e9a250de28b4bf7cb7a7779a9360
cerafinn <74612014+cerafinn@users.noreply.github.com>
2022-03-16 15:23:29 -0400

    pseudocode for testing

--- src/test/java/com/revature/service/ReimbursementServiceTest.java
+++ src/test/java/com/revature/service/ReimbursementServiceTest.java
@@ -1,4 +1,15 @@
 package com.revature.service;
 
 public class ReimbursementServiceTest {
+
+/*
+  To be tested:
+  get all based on userid -- with - for user employee (should not be able to get all)
+  get user specific based on role -- + and - cases, exceptions for incorrect queries
+  get one by id -- + and - cases, exceptions for incorrect queries
+  add one -- + and - , exceptions for invalid inputs
+
+  update only by finance manager for status
+  will a delete be needed?
+  */
 }
--- src/test/java/com/revature/service/UserServiceTest.java
+++ src/test/java/com/revature/service/UserServiceTest.java
@@ -1,4 +1,13 @@
 package com.revature.service;
 
 public class UserServiceTest {
+
+  /*
+  to be tested:
+  get user by id -- will we need a get all route? with tests for querying without a user specified
+  add user -- with additional tests for field restrictions
+  update user -- with additional test for field restrictions
+  delete user  -- expected if there is a user to delete and if there is not
+
+   */
 }
