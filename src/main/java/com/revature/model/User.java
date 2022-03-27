2216844bc642047c54f7804f00b41b50a3106080
cerafinn <74612014+cerafinn@users.noreply.github.com>
2022-03-26 20:33:52 -0400

    completed user model and schema

--- src/main/java/com/revature/model/User.java
+++ src/main/java/com/revature/model/User.java
@@ -1,4 +1,109 @@
 package com.revature.model;
 
+import java.util.Objects;
+
 public class User {
+
+  private int id;
+  private String username;
+  private String password;
+  private String userFirstName;
+  private String userLastName;
+  private String userEmail;
+  private String userRole;
+
+  public User() {
+  }
+
+  public User(int id, String username, String password, String userFirstName, String userLastName, String userEmail, String userRole) {
+    this.id = id;
+    this.username = username;
+    this.password = password;
+    this.userFirstName = userFirstName;
+    this.userLastName = userLastName;
+    this.userEmail = userEmail;
+    this.userRole = userRole;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getUserFirstName() {
+    return userFirstName;
+  }
+
+  public void setUserFirstName(String userFirstName) {
+    this.userFirstName = userFirstName;
+  }
+
+  public String getUserLastName() {
+    return userLastName;
+  }
+
+  public void setUserLastName(String userLastName) {
+    this.userLastName = userLastName;
+  }
+
+  public String getUserEmail() {
+    return userEmail;
+  }
+
+  public void setUserEmail(String userEmail) {
+    this.userEmail = userEmail;
+  }
+
+  public String getUserRole() {
+    return userRole;
+  }
+
+  public void setUserRole(String userRole) {
+    this.userRole = userRole;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if(o == null || getClass() != o.getClass()) return false;
+    User user = (User) o;
+    return id == user.id && Objects.equals(username, user.username) && Objects.equals(password, user.password) && Objects.equals(userFirstName, user.userFirstName) && Objects.equals(userLastName, user.userLastName)  && Objects.equals(userEmail, user.userEmail) && Objects.equals(userRole, user.userRole);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, username, password, userFirstName, userLastName, userEmail, userRole);
+  }
+
+  @Override
+  public String toString() {
+    return "User{" +
+        "id=" + id +
+        ", username='" + username + '\'' +
+        ", password='" + password + '\'' +
+        ", userFirstName='" + userFirstName + '\'' +
+        ", userLastName='" + userLastName + '\'' +
+        ", userEmail='" + userEmail + '\'' +
+        ", userRole='" + userRole + '\'' +
+        '}';
+  }
 }
---
+++ src/schema.sql
@@ -0,0 +1,67 @@
+-- project one schema:
+DROP TABLE IF EXISTS users, user_roles, reimbursement, reimbursement_type, reimbursement_status;
+
+
+--create role table and insert roles
+CREATE TABLE user_roles (
+	id SERIAL PRIMARY KEY,
+	role VARCHAR(20) NOT NULL
+);
+
+INSERT INTO user_roles (role) VALUES
+('employee'),
+('finance manager');
+
+--create user table and insert users, with user_role fk
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY,
+	username VARCHAR(200) NOT NULL UNIQUE,
+	password VARCHAR(200) NOT NULL,
+	user_first_name VARCHAR(200) NOT NULL,
+	user_last_name VARCHAR(200) NOT NULL,
+	user_email VARCHAR(200) NOT NULL,
+	user_role_id INTEGER NOT NULL,
+	CONSTRAINT fk_user_userrole FOREIGN KEY (user_role_id) REFERENCES user_roles(id)
+);
+
+--create reimbursement table, along with remit type and status
+CREATE TABLE reimbursement_type (
+	id SERIAL PRIMARY KEY,
+	type VARCHAR(20) NOT NULL
+);
+
+INSERT INTO reimbursement_type (type) VALUES
+('lodging'),
+('travel'),
+('food'),
+('other');
+
+CREATE TABLE reimbursement_status (
+	id SERIAL PRIMARY KEY,
+	status VARCHAR(20) NOT NULL
+);
+
+INSERT INTO reimbursement_status (status) VALUES
+('pending'),
+('denied'),
+('approved');
+
+CREATE TABLE reimbursement (
+	id SERIAL PRIMARY KEY,
+	reimb_amount INTEGER NOT NULL,
+	reimb_submitted TIMESTAMP NOT NULL,
+	reimb_resolved TIMESTAMP NOT NULL,
+	reimb_description VARCHAR(250) NOT NULL,
+	reimb_author INTEGER NOT NULL,
+	CONSTRAINT fk_reimbursement_reimbauthor FOREIGN KEY (reimb_author) REFERENCES users(id),
+	reimb_resolver INTEGER,
+	CONSTRAINT fk_reimbursement_reimbresolver FOREIGN KEY (reimb_resolver) REFERENCES users(id),
+	reimb_type_id INTEGER NOT NULL,
+	CONSTRAINT fk_reimbursement_reimbtype FOREIGN KEY (reimb_type_id) REFERENCES reimbursement_type(id),
+	reimb_status_id INTEGER NOT NULL,
+	CONSTRAINT fk_reimbursement_reimbstatus FOREIGN KEY (reimb_status_id) REFERENCES reimbursement_status(id)
+);
+
+SELECT * FROM user_roles;
+SELECT * FROM reimbursement_status;
+SELECT * FROM reimbursement_type;
