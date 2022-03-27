83db2613d8548788d691cc8fb935881c7ff50646
cerafinn <74612014+cerafinn@users.noreply.github.com>
2022-03-26 22:17:43 -0400

    added pseudocode to reimbursement dao

--- src/main/java/com/revature/dao/ReimbursementDao.java
+++ src/main/java/com/revature/dao/ReimbursementDao.java
@@ -1,4 +1,12 @@
 package com.revature.dao;
 
 public class ReimbursementDao {
+
+  //add reimbursement
+
+  //view reimbursement -- all, by uid and by status?
+
+  // get remit receipt
+
+  //update reimbursement status
 }
