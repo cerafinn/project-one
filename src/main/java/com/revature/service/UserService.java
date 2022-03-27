c6b9ad56f2a42ee814dda17a74f9420f7c1e8237
cerafinn <74612014+cerafinn@users.noreply.github.com>
2022-03-26 21:10:26 -0400

    added code to userService, moving to DAO layer to create functionality

--- src/main/java/com/revature/service/UserService.java
+++ src/main/java/com/revature/service/UserService.java
@@ -1,4 +1,15 @@
 package com.revature.service;
 
+import com.revature.dao.UserDao;
+
 public class UserService {
+  private UserDao userDao;
+
+  public UserService() {
+    this.userDao = new UserDao();
+  }
+
+  public UserService(UserDao mockDao) {
+    this.userDao = mockDao;
+  }
 }
