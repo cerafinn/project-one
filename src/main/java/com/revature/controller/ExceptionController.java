04dd9f70da9eb3372bf486bd8e85e901dc77efa1
cerafinn <74612014+cerafinn@users.noreply.github.com>
2022-03-28 17:07:06 -0400

    updates to logback, build, gitignore and controller files

--- src/main/java/com/revature/controller/ExceptionController.java
+++ src/main/java/com/revature/controller/ExceptionController.java
@@ -4,10 +4,14 @@ import com.revature.exception.ImageNotFoundException;
 import com.revature.exception.InvalidImageException;
 import io.javalin.Javalin;
 import io.javalin.http.ExceptionHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.security.auth.login.FailedLoginException;
 
 public class ExceptionController implements Controller {
+
+//  private Logger logger = LoggerFactory.getLogger(ExceptionController.class);
 
   private ExceptionHandler<FailedLoginException> failedLogin = (exception, ctx) -> {
     ctx.status(400);
